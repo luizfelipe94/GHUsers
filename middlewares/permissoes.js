89142f58ef92685d0bebbd0fbad4f9bcfdf8da46
Luiz Felipe da Silva <luizfelipe.dasilva94@gmail.com>
2019-08-31 00:15:38 -0300

    init

--- middlewares/permissoes.js
+++ middlewares/permissoes.js
@@ -1,17 +1,17 @@
 module.exports = (...perfis) => {
     return (req, res, next) => {
-        const perfilUsuario = req.user.usuario.perfil;
-        // admin tem acesso a tudo.
-        let autorizado = perfis.some(perfil => perfil === perfilUsuario); 
-        if(autorizado){
-            next();
-        }else{
-            return res.status(401)
-            .json({
-                sucesso: false,
-                menssagem: 'Você não tem permissão para acessar este recurso.'
-            });
-        }
-        // next();
+        // const perfilUsuario = req.user.usuario.perfil;
+        // // admin tem acesso a tudo.
+        // let autorizado = perfis.some(perfil => perfil === perfilUsuario); 
+        // if(autorizado){
+        //     next();
+        // }else{
+        //     return res.status(401)
+        //     .json({
+        //         sucesso: false,
+        //         menssagem: 'Você não tem permissão para acessar este recurso.'
+        //     });
+        // }
+        next();
     }
 }
